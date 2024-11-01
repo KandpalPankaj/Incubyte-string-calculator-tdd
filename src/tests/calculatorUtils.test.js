946d09460cd691e806f74139b4ae992518c450a6
Pankaj Kandpal <pkandpal28@gmail.com>
2024-11-01 12:21:13 +0530

    Add failing test for single input

--- src/tests/calculatorUtils.test.js
+++ src/tests/calculatorUtils.test.js
@@ -3,3 +3,8 @@ import { add } from "../utils/calculatorUtils";
 test("should return 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("should return the number for a single input", () => {
+  expect(add("1")).toBe(1);
+  expect(add("5")).toBe(5);
+});
