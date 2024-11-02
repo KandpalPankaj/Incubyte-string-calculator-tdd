763caa543558e8693fa667bbc126dfb99011b9e8
Pankaj Kandpal <pkandpal28@gmail.com>
2024-11-02 19:44:13 +0530

    Update add function to handle long delimiters

--- src/utils/calculatorUtils.js
+++ src/utils/calculatorUtils.js
@@ -2,13 +2,23 @@ export function add(numbers) {
   if (numbers === "") return 0;
 
   let delimiter = /,|\n/;
+
   if (numbers.startsWith("//")) {
     const parts = numbers.split("\n");
-    delimiter = new RegExp(parts[0][2]);
+
+    const customDelimiters = parts[0]
+      .slice(2)
+      .split(/[[\]]+/)
+      .filter(Boolean);
+    delimiter = new RegExp(customDelimiters.join("|"), "g");
+
     numbers = parts[1];
   }
 
-  const numArray = numbers.split(delimiter).map((num) => parseInt(num, 10));
+  const numArray = numbers
+    .split(delimiter)
+    .map((num) => parseInt(num, 10))
+    .filter(Boolean);
 
   const negatives = numArray.filter((num) => num < 0);
   if (negatives.length > 0) {
